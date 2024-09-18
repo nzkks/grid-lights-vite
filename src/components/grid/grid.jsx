5c9e7037838108d016ac5cc7ed8bb74eae90ce25
nzkks <kkshanthosh@gmail.com>
2024-09-19 11:05:28 +1200

    extract cell as a component

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,9 +1,11 @@
+import Cell from '../cell/cell';
+
 const Grid = ({ gridConfig }) => {
   return (
     <div>
       <div>
         {gridConfig.flat(1).map((cell, i) => {
-          return cell === 1 ? <div key={i}>{cell}</div> : <div key={i} />;
+          return cell === 1 ? <Cell key={i} /> : <div key={i} />;
         })}
       </div>
     </div>
