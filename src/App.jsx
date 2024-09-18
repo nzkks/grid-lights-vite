e36cbd3d439900f8517b459f1b27a13853d23312
nzkks <kkshanthosh@gmail.com>
2024-09-19 10:27:05 +1200

    create a grid matrix config and map through to create the cells

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const gridConfig = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
   return (
     <>
       <h3 style={{ textAlign: 'center' }}>Grid Lights</h3>
+
+      <div>
+        {gridConfig.flat(1).map((cell, i) => {
+          return cell === 1 ? <div key={i}>{cell}</div> : <div key={i} />;
+        })}
+      </div>
     </>
   );
 }
