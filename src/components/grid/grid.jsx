b373e3942e7745646089e86633ef149ecae2eda6
nzkks <kkshanthosh@gmail.com>
2024-09-19 11:49:53 +1200

    Add functionality to activate the cells

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import Cell from '../cell/cell';
 import './grid.css';
 
 const Grid = ({ gridConfig }) => {
-  const activateCell = i => {};
+  const [order, setOrder] = useState([]);
+
+  const activateCells = i => {
+    const newOrder = [...order, i];
+    setOrder(newOrder);
+  };
 
   return (
     <div className="wrapper">
       <div className="grid" style={{ gridTemplateColumns: `repeat(${gridConfig[0].length}, 1fr)` }}>
         {gridConfig.flat(1).map((cell, i) => {
-          return cell === 1 ? <Cell key={i} isActivated={false} onClick={() => activateCell(i)} /> : <div key={i} />;
+          return cell === 1 ? (
+            <Cell key={i} isActivated={order.includes(i)} onClick={() => activateCells(i)} />
+          ) : (
+            <div key={i} />
+          );
         })}
       </div>
     </div>
